0f0a363289189f112b04f4112be30cc0615271a6
Zachary Jaquish <zjaquish@dropbox.com>
2023-11-02 12:00:09 -0700

    Fix import

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/JohnSundell/Files", from: "4.0.0"),
     .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.0.0"),
-    .package(url: "https://github.com/jlaws/SourceKitten.git", .branch("master")),
+    .package(url: "https://github.com/jpsim/SourceKitten.git", .branch("master")),
     .package(url: "https://github.com/surfandneptune/CommandCougar.git", from: "1.0.0"),
     .package(url: "https://github.com/SwiftDocOrg/GraphViz", from: "0.1.1")
   ],
